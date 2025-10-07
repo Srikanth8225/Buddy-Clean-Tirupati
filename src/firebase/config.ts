3d317bc947ee9d61b5020daaa4eb323df8160238
Srikanth Royal <royalsrikanth537@gmail.com>
2025-10-07 07:24:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5770290910-e6f42",
+  "appId": "1:925161430173:web:438e7a9a7685f34105c874",
+  "apiKey": "AIzaSyAxWm4BZ2pBOd4jzCo_TAQFHf4mgpCe6KE",
+  "authDomain": "studio-5770290910-e6f42.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "925161430173"
+};
